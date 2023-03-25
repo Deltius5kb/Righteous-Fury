1c034f3b236934d3b47ae4cf99af926e2afd34cb
Armin Raad <45851390+Deltius5kb@users.noreply.github.com>
2023-03-25 01:22:18 +0000

    started work on server

---
+++ Server/Program.cs
@@ -0,0 +1,7 @@
+﻿using Server;
+
+// See https://aka.ms/new-console-template for more information
+ServerSocket server = new ServerSocket();
+
+await server.Run();
+Console.WriteLine("Server Finished");
---
+++ Server/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace Server;
+
+public class ServerSocket
+{
+    private static int _MAX_NUMBER_OF_CONNECTIONS = 10;
+    private static int _PORT = 5000;
+    private static IPAddress _LOCAL_IP = IPAddress.Parse("127.0.0.1");
+    private IPEndPoint _ENDPOINT;
+    
+    private Socket _listenerSocket;
+    private List<Socket> _clientSockets;
+    
+    public ServerSocket()
+    {
+        _clientSockets = new List<Socket>();
+        _ENDPOINT = new IPEndPoint(_LOCAL_IP, _PORT);
+
+        // AddressFamily.InterNetwork means ipv4
+        _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _listenerSocket.Bind(_ENDPOINT);
+        _listenerSocket.Listen(_MAX_NUMBER_OF_CONNECTIONS);
+        Console.WriteLine("Server started on {0}", _ENDPOINT);
+    }
+
+    public async Task Run()
+    {
+        while (true)
+        {
+            Socket clientSocket = await _listenerSocket.AcceptAsync();
+                
+            _clientSockets.Add(clientSocket);
+            Console.WriteLine("Client connected from {0}", clientSocket.RemoteEndPoint);
+            break;
+        }
+    }
+    
+    public void Close()
+    {
+        for (int i = 0; i < _clientSockets.Count; i++)
+        {
+            _clientSockets[i].Shutdown(SocketShutdown.Both);
+            _clientSockets[i].Close();
+        }
+        
+        _listenerSocket.Close();
+    }
+}
+
